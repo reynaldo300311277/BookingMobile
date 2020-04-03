237a0776a89ba0174cebc73cfaf501a90584f0aa
raoliveira76 <ronaldo_ap_oliveira@yahoo.com.br>
2020-04-02 17:03:35 -0700

    changes in main activity

--- app/src/main/java/com/example/bookingmobile/ActivityPersonalArea.java
+++ app/src/main/java/com/example/bookingmobile/ActivityPersonalArea.java
@@ -5,11 +5,13 @@ import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -92,6 +94,16 @@ public class ActivityPersonalArea extends AppCompatActivity
         catch (Exception e)
         {
         }
+
+        Button btnPersonalNewSearch = findViewById(R.id.btnPersonalNewSearch);
+        btnPersonalNewSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivityPersonalArea.this,
+                        MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
